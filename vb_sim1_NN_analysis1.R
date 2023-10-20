86105c451d7be7ae975709ba5a6ef1faee077421
Alfred <keteralfred@gmail.com>
2023-10-20 11:50:07 +0300

    Commit the Bayesian LCA scripts for NN

---
+++ vb_sim1_NN_analysis1.R
@@ -0,0 +1,116 @@
+#===========================================================================
+#
+#
+# Analysis of simulated data - assuming untested are negative
+#
+#===========================================================================
+
+# setwd("C:\\Users\\Keter\\OneDrive - ITG\\EDCTP\\PhD\\lca\\Vukuzazi\\revised analysis\\vb_sim_analysis\\CC_NN")
+
+#Point in the working directory
+project.path = "/kyukon/scratch/gent/438/vsc43892/vb_sim_analysis"
+# project.path = "C:\\Users\\aketer\\OneDrive - ITG\\EDCTP\\PhD\\lca\\Vukuzazi\\revised analysis\\vb_sim_analysis"
+
+
+
+
+
+#Load the packages
+# install.packages("doSNOW", repos = 'http://R-Forge.R-project.org')
+dynamic.require <- function( package ) {
+  
+  if ( eval( parse( text = paste( 'require(', package, ')' ) ) ) ) {
+    
+    return( 'done' )
+    
+  }
+  
+  install.packages( package )
+  
+  return( eval( parse( text = paste( 'require(', package, ')' ) ) ) )
+  
+}
+
+dynamic.require( 'doParallel' )
+
+dynamic.require( 'rjags' )
+
+dynamic.require( 'random' )
+
+
+
+
+#Read the data
+#Source the dataset
+
+load(file.path(project.path = getwd(),"sim_dat_Final1.RData"))
+
+M = 25
+j = 0
+
+source("data_for_sim1_NN.R")
+# sim1_dat = readRDS(file.path(project.path = getwd(),"sim1_dat.RData"))
+
+
+
+
+
+source("sim_Model_NN.R")
+
+
+
+
+
+
+
+set.seed(79538)
+jags.inits.1 <- function( ) {
+  
+  return( list( beta = rep(rnorm(0,10),3), alpha = rep(rnorm(0,10),3), a_prev = rep(rnorm(0,10),3),
+                .RNG.name = 'lecuyer::RngStream',
+                .RNG.seed = round( 1e+06 * runif( 1 ) ) ) )
+  
+}
+
+# Multi-threaded run
+# Find out how many cores are available (if you don't already know)
+n = detectCores()
+# n
+# Find out how many cores are being used
+# getDoParWorkers()
+# n = M*3
+cl <- makeCluster( n, retry = 5L, sleep = 30.0 ) # change to makeCluster( n ) to use n processes
+registerDoParallel( cl )
+
+# M=100
+
+set.seed( 74123 )
+
+time.1 <- system.time( #getDoParWorkers( )
+  vb.sim1.NN.res1_25 <- foreach( m = 1:M, .inorder = FALSE, 
+                                  .packages = c('R2jags','doParallel'), 
+                                  .multicombine = TRUE) %dopar% {
+             load.module( 'lecuyer' )
+            print(paste("Fitting model",m, sep = ""))
+                                    
+             result = jags.parallel(data = sim1_dat[[m]], inits = jags.inits.1,
+                      parameters.to.save = c("beta","alpha","a_prev","se","sp","pi"),
+                      model.file = 'sim_model1_NN.txt', n.chain = 3, 
+                      n.iter = 50000, n.burnin = 25000,
+                      n.thin = 10, DIC = T, jags.module = "dic", n.cluster = 3)
+               return( result )
+                       }
+)
+print(time.1)
+
+
+
+stopCluster(cl)
+
+
+
+saveRDS(vb.sim1.NN.res1_25, file = "/kyukon/data/gent/438/vsc43892/vb_sim_analysis/vb.sim1.NN.res1_25.RData")
+# saveRDS(vb.sim1.NN.res1_100, file = "C:\\Users\\aketer\\OneDrive - ITG\\EDCTP\\PhD\\lca\\Vukuzazi\\revised analysis\\vb_sim_analysis\\vb.sim1.NN.res1_100.RData")
+
+
+# print( vb.sim1.NN.res1_25, intervals = c(0.5, 0.025, 0.975))
